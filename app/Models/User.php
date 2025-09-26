d56aa4313d257cc16204fac093da8c16fc1dfbfb
Elias <edemoraesneto@us.ci.org>
2025-09-26 15:24:47 -0300

    Working with tests, add new relationships, models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'email_verified_at',
+        'remember_token',
+        'updated_at',
+        'created_at',
     ];
 
     /**
